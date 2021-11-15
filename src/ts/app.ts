a447b17023cf485c1469875361ef84f570c692dd
MktCoto <helly.frank.tang@gmail.com>
2021-11-15 13:35:51 +0100

    add app.js/ app.ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,8 +1,25 @@
-function app() : void
+/* function app() : void
 {
     console.log('connected');
 }
 
 app();
 
-export default app;
+export default app; */
+
+const roulette = document.querySelector('#roulette');
+
+roulette.addEventListener('click',girar);
+
+function girar(){
+
+    let rand= Math.random()*7200;
+    calcular(rand);
+
+    function calcular(rand){
+        valor = rand/360;
+
+    }
+
+
+}
