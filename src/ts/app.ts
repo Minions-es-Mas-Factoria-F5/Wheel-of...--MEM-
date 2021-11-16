cc61ac37a1c80fe03f4df12d83c2c54a99309747
Emma <emmafecor@hotmail.com>
2021-11-16 13:11:53 +0100

    some js changes

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,15 +1,17 @@
-/* function app() : void
+function app() : void
 {
     console.log('connected');
 }
 
 app();
 
-<<<<<<< HEAD
 export default app;
 
 
-const participantsList = ["Carla", "Giacomo", "Heli", "Ignacio", "Ileen", "Emma" ];
+const participantsList = ["Carla", "Giacomo", "Heli", "Ignacio", "Ileen", "Emma"];
+
+
+
 
 /* 
 const participantsList = ["Agustín", "Alba", "Ana", "Áxel", "Carla", "Caio", 
@@ -17,23 +19,21 @@ const participantsList = ["Agustín", "Alba", "Ana", "Áxel", "Carla", "Caio",
 "Heli", "Ignacio", "Ileen", "Irina", "Jorge", "María F.", "María", "Marlene", 
 "Migedelis", "Omar", "Rebeca", "Rubén", "Raúl", "Sara", "Sasha", "Vivian"];
  */
-=======
-export default app; */
 
-/* const roulette = document.querySelector('#roulette');
+/* function printArrayInRoulette (){
+    let rouletteList = participantsList.concat();
+     -- let rouletteList = participantsList.toString(); -- 
+    console.log(rouletteList);
+    document.getElementById("roulette-names").innerHTML = `${rouletteList}`;
+}
 
-roulette.addEventListener('click',girar);
+printArrayInRoulette ()
 
-function girar(){
+function chooseRandomParticipant (){
+   let chosenParticipant = participantsList[Math.floor(Math.random()*participantsList.length)];
+   console.log(chosenParticipant);
+   document.getElementById("winnerPopUp").innerHTML = `${chosenParticipant}`;
+}
 
-    let rand= Math.random()*7200;
-    calcular(rand);
+chooseRandomParticipant () */
 
-    function calcular(rand){
-        valor = rand/360;
-
-    }
-
-
-} */
->>>>>>> 7527af3a21f02956aab7c5d3c6d0099da8b325bf
