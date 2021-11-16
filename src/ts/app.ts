d406858b231a5386b7baf27361d85b5e23b3ddc2
Emma <emmafecor@hotmail.com>
2021-11-16 11:23:58 +0100

    adding array of participants to ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,3 +6,13 @@ function app() : void
 app();
 
 export default app;
+
+
+const participantsList = ["Carla", "Giacomo", "Heli", "Ignacio", "Ileen", "Emma" ];
+
+/* 
+const participantsList = ["Agustín", "Alba", "Ana", "Áxel", "Carla", "Caio", 
+"Christian", "Daniel", "Diego", "Elena", "Emma", "Francisco", "Giacomo", 
+"Heli", "Ignacio", "Ileen", "Irina", "Jorge", "María F.", "María", "Marlene", 
+"Migedelis", "Omar", "Rebeca", "Rubén", "Raúl", "Sara", "Sasha", "Vivian"];
+ */
