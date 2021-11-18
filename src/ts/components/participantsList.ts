6ba29e148303a0ab16bc5f621b03ce2f19af31b6
Ileenfdz <ileenfdz@gmail.com>
2021-11-18 14:08:35 +0100

    added something

--- src/ts/components/participantsList.ts
+++ src/ts/components/participantsList.ts
@@ -1,14 +1,15 @@
 let list:any[] = [];
 const inputParticipants = (<HTMLInputElement>document.getElementById('input-add-participants'));
 const btnAddParticipants = (<HTMLInputElement>document.getElementById('btn-add-participants'));
-          
+
 export function addParticipants(){
 
-    if (inputParticipants.value !== '' && list.includes(inputParticipants.value) == false) {
-       list.push(inputParticipants.value);
-       console.log(list); 
-    }else{
-        inputParticipants.value = '';
+    let participantsValue= inputParticipants.value;
+
+    if (participantsValue !== '' && list.includes(participantsValue) == false) {
+       list.push(participantsValue);
+       console.log(list);
+          inputParticipants.value = '';
         inputParticipants.placeholder = 'Insert a valid name';
         console.log('connected 2');
     }
